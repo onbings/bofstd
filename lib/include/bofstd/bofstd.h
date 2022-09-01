4cf5d35cc7504458b96b9c1970c329d7158dbb96
bha <b.harmel@evs.com>
2022-09-01 14:58:31 +0200

    a

--- lib/include/bofstd/bofstd.h
+++ lib/include/bofstd/bofstd.h
@@ -345,7 +345,7 @@ class BOFSTD_EXPORT BofException : public std::exception
 public:
   BofException(std::string _Header_S, std::string _Context_S = "", std::string _Where_S = "", int32_t _ErrorCode_S32 = 0);
   const char *what(); //throw ();
-  int f(int);
+
 private:
   std::string mHeader_S;
   BOFERR mErrorCode_E;
