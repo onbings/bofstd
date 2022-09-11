c877e33c88558cd2f425c1c48b386fedce3f37a1
bha <b.harmel@evs.com>
2022-09-11 16:30:53 +0200

    5.1.15.4

--- lib/include/bofstd/bofstd.h
+++ lib/include/bofstd/bofstd.h
@@ -76,45 +76,6 @@ extern BOFSTDPARAM GL_BofStdParam_X;
 
 /*** Definitions *************************************************************/
 
-#if defined __linux__
-#if ((__GNUC__ < 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ < 9)))  //For H3XP platform for example
-#include<memory>
-namespace std {
-  template< typename T> struct _Unique_if
-  {
-    typedef unique_ptr<T> _Single_object;
-  };
-
-  template< typename T> struct _Unique_if<T[]>
-  {
-    typedef unique_ptr<T[]> _Unknown_bound;
-  };
-
-  template< typename T, size_t N> struct _Unique_if<T[N]>
-  {
-    typedef void _Known_bound;
-  };
-
-  template< typename T, class... Args>
-  typename _Unique_if<T>::_Single_object
-    make_unique(Args&&... args) {
-    return unique_ptr<T>(new T(std::forward<Args>(args)...));
-  }
-
-  template< typename T>
-  typename _Unique_if<T>::_Unknown_bound
-    make_unique(size_t n) {
-    typedef typename remove_extent<T>::type U;
-    return unique_ptr<T>(new U[n]());
-  }
-
-  template< typename T, class... Args>
-  typename _Unique_if<T>::_Known_bound
-    make_unique(Args&&...) = delete;
-}
-#endif
-#endif
-
 #define BOF                         onbings::bof
 #define BEGIN_BOF_NAMESPACE()       namespace onbings { namespace bof {
 #define END_BOF_NAMESPACE()         } }
