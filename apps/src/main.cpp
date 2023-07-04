0d84d673e42083b32b348be97ef9b425e33e8168
HARMEL, Bernard <b.harmel@evs.com>
2023-07-04 13:44:17 +0200

    Add bofstd app

--- apps/src/main.cpp
+++ apps/src/main.cpp
@@ -1,9 +1,18 @@
+#include <bofstd/boffs.h>
 #include <stdio.h>
-#include <bofstd/bofstd.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  printf("Hello world, I'm bofstd version %s\n", Bof_GetVersion().c_str());
-  return 0
-}
+  BOF::BOFSTDPARAM StdParam_X;
+  std::string Cwd_S;
 
+  StdParam_X.AssertInRelease_B = true;
+  StdParam_X.AssertCallback = nullptr;
+  if (Bof_Initialize(StdParam_X) == BOF_ERR_NO_ERROR)
+  {
+    BOF::Bof_GetCurrentDirectory(Cwd_S);
+    printf("\nPwd %s\nRunning BofStd V %s on %s under %s\n", Cwd_S.c_str(), StdParam_X.Version_S.c_str(), StdParam_X.ComputerName_S.c_str(), StdParam_X.OsName_S.c_str());
+    BOF::Bof_Shutdown();
+  }
+  return 0;
+}
