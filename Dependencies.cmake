f37e0e0c49ef7c0435bd1837f56710c56e1f41a1
HARMEL, Bernard <b.harmel@evs.com>
2025-05-21 12:24:10 +0200

    V 6.1.2.3 Remove json and xml support Remove miniaudio remove vcpkg depen Remove multimedia file

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -27,23 +27,23 @@ endif()
 # ===========================
 # == REQUIRED DEPENDENCIES ==
 # ===========================
-if(NOT TARGET jsoncpp_object)
-  find_package_dependency(jsoncpp REQUIRED)
-
-  if(TARGET jsoncpp_lib)
-    add_library(jsoncpp::jsoncpp ALIAS jsoncpp_lib)
-  elseif(TARGET jsoncpp_static)
-    add_library(jsoncpp::jsoncpp ALIAS jsoncpp_static)
-  else()
-    message(FATAL_ERROR "Unknown target for jsoncpp")
-  endif()
-endif()
+##if(NOT TARGET jsoncpp_object)
+##  find_package_dependency(jsoncpp REQUIRED)
+##
+##  if(TARGET jsoncpp_lib)
+##    add_library(jsoncpp::jsoncpp ALIAS jsoncpp_lib)
+##  elseif(TARGET jsoncpp_static)
+##    add_library(jsoncpp::jsoncpp ALIAS jsoncpp_static)
+##  else()
+##    message(FATAL_ERROR "Unknown target for jsoncpp")
+##  endif()
+##endif()
 
 #just above find_package_dependency(jsoncpp REQUIRED)
 
 ####find_package_dependency(spdlog REQUIRED)
 #find_package_dependency(fmt REQUIRED)
-find_package_dependency(date REQUIRED)
+#find_package_dependency(date REQUIRED)
 
 #if you want to use readerwriterqueue or concurrentqueue add the following to vcpkg.json
 #    {"name": "readerwriterqueue"},
